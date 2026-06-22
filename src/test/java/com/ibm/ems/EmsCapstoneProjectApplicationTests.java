774bb2322b969f4c5b1237db419563a8584ce90c
avianage <aakash.joshi.cs@gmail.com>
2026-06-22 16:07:20 +0530

    Initial Commit

---
+++ src/test/java/com/ibm/ems/EmsCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
